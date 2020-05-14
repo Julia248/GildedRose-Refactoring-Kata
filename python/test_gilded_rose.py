8a71eea89e1930e23638a6871b02463da09d200a
956258 <julia.foerster@pwc.com>
2020-05-14 09:08:38 +0100

    added test sell_in_decreases_when_day_passed_without_sell

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -6,25 +6,31 @@ from gilded_rose import Item, GildedRose
 class GildedRoseTest(unittest.TestCase):
 
     def test_regular_items_decrease_by_one(self):
-        items = [Item("chocolate", 1, 3)]
+        items = [Item.create_item("chocolate", 1, 3)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals(items[0].quality, 2)
 
     def test_quality_goes_up_for_improving_products_aged_brie(self):
-        items = [Item("Aged Brie", 1, 1)]
+        items = [Item.create_item("Aged Brie", 1, 1)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals(items[0].quality, 2)
 
+    def sell_in_decreases_when_day_passed_without_sell(self):
+        items = [Item.create_item("Aged Brie", 1, 1)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(items[0].sell_in, 0)
+
     def test_quality_goes_up_for_improving_products_backstage_passes(self):
-        items = [Item("Backstage passes to a TAFKAL80ETC concert", 16, 1)]
+        items = [Item.create_item("Backstage passes to a TAFKAL80ETC concert", 16, 1)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals(items[0].quality, 2)
 
     def test_quality_goes_up_by_two_for_improving_products_with_10_days_or_less_left_backstage_passes(self):
-        items = [Item("Backstage passes to a TAFKAL80ETC concert", 8, 18)]
+        items = [Item.create_item("Backstage passes to a TAFKAL80ETC concert", 8, 18)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals(items[0].quality, 20)
@@ -36,34 +42,34 @@ class GildedRoseTest(unittest.TestCase):
     #     self.assertEquals(items[0].quality, 20)
 
     def test_quality_goes_up_by_three_for_improving_products_with_5_days_or_less_left(self):
-        items = [Item("Backstage passes to a TAFKAL80ETC concert", 4, 18)]
+        items = [Item.create_item("Backstage passes to a TAFKAL80ETC concert", 4, 18)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals(items[0].quality, 21)
 
     def test_quality_decrease_twice_as_fast_after_sell_by(self):
-        items = [Item("Chocolata", 0, 18)]
+        items = [Item.create_item("Chocolata", 0, 18)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals(items[0].quality, 16)
 
 
     def test_backstage_passes_and_brie_go_to_quality_zero_after_sell_by(self):
-        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 18)]
+        items = [Item.create_item("Backstage passes to a TAFKAL80ETC concert", 0, 18)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals(items[0].quality, 0)
 
 
     def test_sulfuras_neverSold(self):
-        items = [Item("Sulfuras, Hand of Ragnaros", 1, 18)]
+        items = [Item.create_item("Sulfuras, Hand of Ragnaros", 1, 18)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals(items[0].quality, 18)
         self.assertEquals(items[0].sell_in, 1)
 
     def test_quality_does_not_increase_past_50(self):
-        items = [Item("Aged Brie", 10, 49)]
+        items = [Item.create_item("Aged Brie", 10, 49)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         gilded_rose.update_quality()
